3baa9f2a60c03917543ce4dee9ad92e736a67e62
balbano80 <balbano80@gmail.com>
2018-05-26 12:04:12 -0700

    working version for Customer part of project

--- bamazon.sql
+++ bamazon.sql
@@ -2,26 +2,29 @@ DROP DATABASE IF EXISTS bamazonDB;
 
 CREATE DATABASE bamazonDB;
 
-USE bamazon;
+USE bamazonDB;
 
 CREATE TABLE products(
-item_id INT(11) AUTO_INCREMENT NOT NULL;
-product_name VARCHAR(30) NOT NULL;
-department_name VARCHAR(30);
-price DECIMAL(10, 2) DEFAULT 0.00;
-stock_quantity INT(11) DEFAULT 0;
+item_id INT(11) NOT NULL AUTO_INCREMENT, 
+product_name VARCHAR(40) NOT NULL,
+department_name VARCHAR(30),
+price DECIMAL(10, 2) DEFAULT 0.00,
+stock_quantity INT(11) DEFAULT 0,
+PRIMARY KEY (item_id)
 );
 
 INSERT INTO  products (product_name, department_name, price, stock_quantity)
-VALUES ("", "", , ), 
-    ("", "", , ), 
-    ("", "", , ), 
-    ("", "", , ), 
-    ("", "", , ), 
-    ("", "", , ), 
-    ("", "", , ), 
-    ("", "", , ), 
-    ("", "", , ), 
-    ("", "", , ), 
-    ("", "", , ), 
-    ("", "", , );
+VALUES ("Sony PS4", "Electronics", 399.99, 20), 
+    ("Hue Light Kit", "Electronics", 79.99, 10), 
+    ("32 inch Computer Monitor", "Electronics", 199.99, 15), 
+    ("16.9 fl oz Water(24 pack)", "Grocery", 3.99, 100), 
+    ("Chicken Noodle Soup(8 pack", "Grocery", 10.99, 40), 
+    ("Macaroni and Cheese(5 pack)", "Grocery", 5.99, 25), 
+    ("Crew socks(6 pack)", "Clothing", 12.99, 50), 
+    ("Men's plain white T-shirts(5 pack)", "Clothing", 19.99, 60), 
+    ("Flannel pajama pants(3 pack)", "Clothing", 24.99, 30), 
+    ("Body wash", "Beauty and Health", 6.99, 40), 
+    ("Toothpaste(2 pack)", "Beauty and Health", 4.99, 90), 
+    ("Shaving cream", "Beauty and Health", 2.99, 60);
+
+    -- SELECT * FROM products;
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,9 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var AsciiTable = require("ascii-table");
+require("colors");
 
+var totalPurchase = 0;
 var connection = mysql.createConnection({
     host: "localhost",
   
@@ -19,18 +22,107 @@ connection.connect(function(err) {
     if (err){
         throw err;
     } 
-    console.log("connected as id " + connection.threadId + "\n");
+    // console.log("connected as id " + connection.threadId + "\n");
     readDB();
-  });
+  });  // establishes connection to database and fires off readDB function
+
+function buyProducts(){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "product",
+            message: "Please enter in the id of product you would like to buy: ",
+            validate: function(value){
+                if (!isNaN(parseInt(value))){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "quantity",
+            message: "How many would you like to buy? ",
+            validate: function(value){
+                if (!isNaN(parseInt(value)) || parseInt(value) <= 0){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+    ]).then(function(response){
+        quantityUpdate(response.product, parseInt(response.quantity));
+    })
+}; // asks user what item number they would like to purchase and the quantity
+   // validates for both that they are a number and not negative
+   // fires off quantityUpdate function with user's responses as arguments
 
 function readDB(){
-    connection.query( "SELECT * FROM bamazonDB", function(err, res){
+    connection.query( "SELECT * FROM products", function(err, res){
         if (err){
             throw err;
         }
-        console.log(res);
-        connection.end(); 
+        var table = new AsciiTable("Products");
+        table
+            .setHeading("id", "Product Name", "Department Name", "Price", "Quantity");
+            for (var i = 0; i < res.length; i++){
+                table.addRow(res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity);   
+            }
+        console.log(table.toString());
+        buyProducts();
     })
-} // reads DB and displays all content in terinal
 
-//TODO use a table displaying package to pretty it up
+}; // reads from DB, displays all content in terminal and fires off buyProducts function
+
+function quantityUpdate(id, quantity){
+    connection.query("SELECT stock_quantity, price FROM products WHERE item_id =?", [id], function(err, results){
+        // console.log(results[0].stock_quantity);
+        // console.log(results[0].price);
+        if (quantity > results[0].stock_quantity){
+            console.log("Insufficient Inventory!".red);
+            console.log("Please try again from the below inventory.")
+            console.log("");
+            readDB();
+        }
+        else{
+            var newQuantity = results[0].stock_quantity - quantity;
+            totalPurchase += (quantity * results[0].price);
+            plusTax = totalPurchase * 1.0875;
+            connection.query("UPDATE products SET stock_quantity=? WHERE item_id =?", [newQuantity, id], function(err, results){
+                if (err) throw err;
+                console.log("Purchase successful".green);
+                 console.log("Total purchase(with tax): $" + plusTax.toFixed(2));
+                 console.log("- - - - - - - - - - - - - - - - - - - - - - - - - - - - -".rainbow);
+                 console.log("");
+                anotherPurchase();
+            })
+        }
+    });
+}; // checks if quantity passed through is higher or lower than inventory for user selected item id
+   // if higher, lets user know not enough inventory and brings up inventory and purchase option again(readDB function)
+   // else, subtracts user quantity from inventory number and updates the inventory at that item id
+    // also informs the user of their total purchase so far, including tax
+    // fires anotherPurchase function 
+
+function anotherPurchase(){
+    inquirer.prompt({
+        type: "confirm",
+        name: "another",
+        message: "Would you like to make another purchase?"
+        }
+    ).then(function(res){
+        if (res.another === true){
+            readDB();
+        }
+        else{
+            connection.end();
+            return 0;
+        }
+    });
+}; // runs inquirer to ask if they want to make another purchase
+   // if yes, runs through readDB function again
+   //else, terminates connection to DB and ends application
